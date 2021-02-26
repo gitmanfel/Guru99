140a122b6cae79621e7681e94c177c7ed72408be
Felicien MANZI <35733583+gitmanfel@users.noreply.github.com>
2021-02-26 23:11:16 +0100

    php

---
+++ Customerhomepage.php
@@ -0,0 +1,46 @@
+<html>
+<!-- comments:Application home page-->
+
+<head>
+    <!--comments: To link to a common cascading style sheets-->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <!--comments: To link to javascript files-->
+    <script language="JavaScript" src="../JavaScript/test.js"></script>
+    <div>
+        <h2 class="barone">Guru99 Bank</h2>
+    </div>
+    <title> Guru99 Bank Customer HomePage </title>
+</head>
+<div>
+    <div>
+        <ul class="menusubnav">
+            <li class="orange"><a href="Customerhomepage.php">Customer</a></li>
+            <li><a href="BalEnqInput.php">Balance Enquiry</a></li>
+            <li><a href="customerfundinput.php">Fund Transfer</a></li>
+            <li><a href="PasswordInput.php">Changepassword</a></li>
+            <li><a href="MiniStatementInput.php">Mini Statement</a></li>
+            <li><a href="Logout.php">Log out</a></li>
+        </ul>
+    </div>
+</div>
+<!--comments: home page body-->
+
+<body>
+    <table class="layout1" border="0" align="center">
+        <td>
+            <table align="center" width="70%">
+                <center><img src="../images/1.gif"><img src="../images/3.gif"><img src="../images/2.gif"></center>
+                </tr>
+                <!--comments: To link all the screens-->
+                <tr>
+                    <td>
+                        <marquee class="heading3" behavior="alternate">Welcome To Customer's Page of Guru99 Bank
+                        </marquee>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </table>
+</body>
+
+</html>
